644595bb6c3324df24cc1beb990ea05ddd0ba1b0
Arundhati-Bandyopadhyay <77777188+Arundhati-Bandyopadhyay@users.noreply.github.com>
2023-01-08 22:07:35 +0530

    updated

--- express/app.js
+++ express/app.js
@@ -1,33 +1,20 @@
-const express = require('express');
-const bodyParser=require('body-parser');
+const path = require('path');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded());
 
-app.use('/',(req, res, next) => {
-        console.log('this always runs!');
-        
-        next(); // Allows the request to continue to the next middleware in line
-      });
-      
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product',(req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product></form>');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')))
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
-app.use('/product',(req,res,next)=>{
-  console.log(req.body);
-res.redirect('/');
-})
-
-app.use('/',(req, res, next) => {
-  console.log('In the middleware!');
-  //res.send('<h1>Hello from Express!</h1>');
-  //next(); // Allows the request to continue to the next middleware in line
-});
-
 
 app.listen(3000);
